57fd15a5b9fd13de8c21f5e187963d24346665a2
Meco Man <920369182@qq.com>
2024-10-08 00:14:57 -0400

    add RTduino plot files

---
+++ Processors/TFT_eSPI_RTduino.h
@@ -0,0 +1,11 @@
+        ////////////////////////////////////////////////////
+        //       TFT_eSPI RTduino driver functions        //
+        ////////////////////////////////////////////////////
+
+// This is a driver for RTduino boards, it supports SPI interface displays
+// 8-bit parallel interface to TFT
+
+#ifndef _TFT_eSPI_RTDUINO_
+#define _TFT_eSPI_RTDUINO_
+
+#endif
